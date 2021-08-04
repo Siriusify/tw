0b34aba51e2cb334cb44e86971c17eebd2954cf7
Siriusify <42270766+Siriusify@users.noreply.github.com>
2021-08-04 11:57:35 +0300

    Add files via upload

---
+++ rararan.py
@@ -0,0 +1,4 @@
+from TwitchChannelPointsMiner import TwitchChannelPointsMiner
+twitch_miner = TwitchChannelPointsMiner("D1nocan")
+twitch_miner.analytics(host="127.0.0.1", port=5000, refresh=5)
+twitch_miner.mine(["apuugee"])   
