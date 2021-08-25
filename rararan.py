f4693847fef7a110c88b17b875ef3da5cd95c162
Siriusify <42270766+Siriusify@users.noreply.github.com>
2021-08-25 16:33:15 +0300

    Update rararan.py

--- rararan.py
+++ rararan.py
@@ -1,4 +1,4 @@
 from TwitchChannelPointsMiner.TwitchChannelPointsMiner import TwitchChannelPointsMiner
 twitch_miner = TwitchChannelPointsMiner("D1nocan")
 twitch_miner.analytics(host="127.0.0.1", port=5000, refresh=5)
-twitch_miner.mine(["roshtein", "vondice", "deuceace"])   
+twitch_miner.mine(["roshtein", "1001surat", "deuceace"])   
